294acf04c0c23b2f394df4b4c3b50dbc58398d74
szukiyu <y_suzuki@tome.jp>
2016-06-27 15:47:38 -0700

    updated 0628

--- tf.R
+++ tf.R
@@ -7,8 +7,46 @@ registerPlugin("loader", plug )
 helloworld<- cxxfunction(
   signature(xIn="numeric"),
   body <- '
-  using namespace std;
-  sample();
+  
+  
+  #include <fstream> 
+  #include <iostream>
+  #include <iomanip>
+  #include <sstream>
+  #include <string>
+  #include <vector>
+  #include <iterator>
+  #include <memory>
+  #include <stdio.h>
+ 
+
+  using namespace tensorflow;
+  
+  int b_size = 15; // number of minibatch                                                                 
+  int n_step = 16; // size of minibatch                                                                   
+
+  std::vector<std::vector<double> > X_test_norm;
+  std::vector<double> y_test_norm;
+  load_dataset("/home/suzuki/LSTM_tsc-master/data/TEST_batch2000", X_test_norm, y_test_norm);
+  
+  // Initialize a tensorflow session                                                                      
+  Session* session;
+  Status status = NewSession(SessionOptions(), &session);
+  if (!status.ok()) {
+    std::cout < < status.ToString() < < std::endl;
+    return 1;
+  }
+
+  // Read in the protobuf graph we exported                                                               
+  GraphDef graph_def;
+  status = ReadBinaryProto(Env::Default(), "/home/suzuki/LSTM_tsc-master/models/output_graph.pb", &graph_def);
+  if (!status.ok()) {
+    std::cout < < status.ToString() < < std::endl;
+    return 1;
+  }
+
+
   return xIn;
    ', 
-  plugin="hello")
+  includes=c("#include <vector>", '#include "tensorflow/core/public/session.h" ',  '#include "tensorflow/core/platform/env."'),
+  plugin="loader")
